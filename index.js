e74c30feb22208fd1fd05214e935b59f21e0b11a
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-30 12:09:38 +0200

    Аdd all libraries to Project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+
+const PORT = 5000
+
+app.get('/', (req, res)=>{
+    res.send('Hello from express');
+})
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
